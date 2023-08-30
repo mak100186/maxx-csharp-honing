2d921bea2799ba23916b60ca048a268d0f595718
Muhammed Khan <muhammed.khan@kindredgroup.com>
2023-08-30 11:59:52 +1000

    added type loading through reflection and added median sorted arrays algo as well

--- Maxx.CSharp.Honing/AlgorithmFactory.cs
+++ Maxx.CSharp.Honing/AlgorithmFactory.cs
@@ -1,16 +1,29 @@
-﻿using Maxx.CSharp.Honing.Algorithms;
+﻿using System.Reflection;
+
+using Maxx.CSharp.Honing.Algorithms;
 
 namespace Maxx.CSharp.Honing;
 
 public static class AlgorithmFactory
 {
-    public static List<string> ExistingAlgorithms = new() { nameof(IsSubsequence) };
+    public static List<string> ExistingAlgorithms = 
+        GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Maxx.CSharp.Honing.Algorithms")
+        .Select(t => t.Name)
+        .ToList();
+
     public static IAlgorithm GetAlgorithm(string algorithmName)
     {
-        return algorithmName switch
-        {
-            nameof(IsSubsequence) => new IsSubsequence(),
-            _ => throw new NotImplementedException(),
-        };
+        var types = GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Maxx.CSharp.Honing.Algorithms");
+        var type = types.Single(t => t.Name == algorithmName);
+
+        return (IAlgorithm)Activator.CreateInstance(type);
+    }
+
+    private static Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+    {
+        return 
+            assembly.GetTypes()
+                .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                .ToArray();
     }
 }
---
+++ Maxx.CSharp.Honing/Algorithms/FindMedianSortedArrays.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+
+namespace Maxx.CSharp.Honing.Algorithms;
+public class FindMedianSortedArrays : IAlgorithm
+{
+    public void Run()
+    {
+        Execute(new[] { 1, 3 }, new[] { 2 }).ShouldBe(2.0f);
+    }
+
+    public double Execute(int[] front, int[] back)
+    {
+        int[] combined = new int[front.Length + back.Length];
+        Array.Copy(front, combined, front.Length);
+        Array.Copy(back, 0, combined, front.Length, back.Length);
+
+        Array.Sort(combined);
+
+        if (combined.Length == 1)
+        {
+            return combined[0];
+        }
+
+        if (combined.Length % 2 == 0) //even
+        {
+            var leftIndex = combined.Length / 2 - 1;
+            var rightIndex = combined.Length / 2;
+
+            return (combined[leftIndex] + combined[rightIndex]) / 2.0f;
+        }
+        else
+        {
+            var midIndex = (combined.Length - 1) / 2;
+            return combined[midIndex];
+        }
+    }
+}
