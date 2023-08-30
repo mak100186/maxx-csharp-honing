af75c5244d9c0ca847b03697734c4f5956d15fd7
Muhammed Khan <muhammed.khan@kindredgroup.com>
2023-08-30 14:19:48 +1000

    better factory

--- Maxx.CSharp.Honing/AlgorithmFactory.cs
+++ Maxx.CSharp.Honing/AlgorithmFactory.cs
@@ -1,27 +1,29 @@
 ﻿using System.Reflection;
 
-using Maxx.CSharp.Honing.Algorithms;
-
 namespace Maxx.CSharp.Honing;
 
 public static class AlgorithmFactory
 {
-    public static List<string> ExistingAlgorithms = 
-        GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Maxx.CSharp.Honing.Algorithms")
-        .Select(t => t.Name)
-        .ToList();
+    public static IEnumerable<Type> AllAlgorithmTypes =
+        GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Maxx.CSharp.Honing.Algorithms");
+
+    public static IEnumerable<string> AllAlgorithmNames =
+        AllAlgorithmTypes.Select(t => t.Name);
+
+    public static Type GetAlgorithmTypeByName(string algorithmName)
+    {
+        return AllAlgorithmTypes.Single(t => t.Name == algorithmName);
+    }
 
     public static IAlgorithm GetAlgorithm(string algorithmName)
     {
-        var types = GetTypesInNamespace(Assembly.GetExecutingAssembly(), "Maxx.CSharp.Honing.Algorithms");
-        var type = types.Single(t => t.Name == algorithmName);
-
-        return (IAlgorithm)Activator.CreateInstance(type);
+        return (IAlgorithm)Activator
+            .CreateInstance(GetAlgorithmTypeByName(algorithmName));
     }
 
     private static Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
     {
-        return 
+        return
             assembly.GetTypes()
                 .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
                 .ToArray();
--- Maxx.CSharp.Honing/Program.cs
+++ Maxx.CSharp.Honing/Program.cs
@@ -7,7 +7,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Available algorithms:");
-        Console.WriteLine(string.Join('\n', AlgorithmFactory.ExistingAlgorithms));
+        Console.WriteLine(string.Join('\n', AlgorithmFactory.AllAlgorithmNames));
 
         Console.WriteLine("\nWhich algorithm would you like to run:");
         var algorithmName = Console.ReadLine();
